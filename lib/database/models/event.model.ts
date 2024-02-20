a3e9400b887c40c7581b9a6a36f1cee7b934aeba
pipidepulus <pipidepulus@hotmail.com>
2024-02-19 21:50:23 -0500

    5th  commit

--- lib/database/models/event.model.ts
+++ lib/database/models/event.model.ts
@@ -9,7 +9,7 @@ export interface IEvent extends Document {
   imageUrl: string;
   startDateTime: Date;
   endDateTime: Date;
-  price?: string;
+  price?: number;
   isFree: boolean;
   url?: string;
   category: { _id: string; name: string };
@@ -24,7 +24,7 @@ const EventSchema = new Schema({
   imageUrl: { type: String, required: true },
   startDateTime: { type: Date, default: Date.now },
   endDateTime: { type: Date, default: Date.now },
-  price: { type: String },
+  price: { type: Number },
   isFree: { type: Boolean, default: false },
   url: { type: String },
   category: { type: Schema.Types.ObjectId, ref: "Category" },
